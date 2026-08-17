0b1f3f5f0aa22ec2fe5933262ccccfa71c638d7b
Atharva Singh <Atharvasingh214@gmail.com>
2026-08-18 00:22:59 +0530

    Time: 137 ms (24.7%), Space: 18.4 MB (49.13%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -7,10 +7,10 @@ class Solution(object):
         """
         row=0
         col=len(matrix[0])-1
-        while(row<len(matrix) and col>=0):
+        while(row<len(matrix) and col>=0):#row se bahar na jaaye and col>0
             if matrix[row][col]==target:
                 return True
-            elif matrix[row][col]>target:
+            elif matrix[row][col]>target:#order aisa hai ki left jayega to smaller number and niche toh bada number
                 col=col-1
             else:
                 row =row+1
