aca71dd49f0733cbba75636b9a51dc32fff436c3
Atharva Singh <Atharvasingh214@gmail.com>
2026-09-09 13:31:31 +0700

    Time: 4 ms (79.37%), Space: 13.7 MB (19.97%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        #arr=[]
+        #for i in range (0,len(nums)):
+        #    if nums[i]!=0:
+        #        arr.append(nums[i])
+        #n=len(nums)-len(arr)
+        #for i in range (n):
+        #    arr.append(0)
+        #for i in range(len(nums)):
+        #    nums[i]=arr[i]
+        #but this will not work kyuki that you must do this in-place without making a copy of the array
+        
+        # ab kya kaare 2 pointer se array overwrite krenge
+        pos=0
+        for i in range(0,len(nums)):
+            if nums[i]!=0:
+                nums[pos]=nums[i]
+                pos+=1
+        for i in range(pos,len(nums)):
+            nums[i]=0
