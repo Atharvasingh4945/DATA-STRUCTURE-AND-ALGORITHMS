2b03c94411187aefda7ef7359320a64a00888ce7
Atharva Singh <Atharvasingh214@gmail.com>
2026-09-16 11:10:40 +0530

    Time: 7 ms (19.52%), Space: 13.9 MB (43.88%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #result=[]
+        count=0
+        for i in range(0,len(nums)):
+            if i==0 or nums[i]!=nums[i-1]:
+                #result.append(nums[i])
+                nums[count]=nums[i]
+                count+=1
+        return count
+                
+# seee phle laga meko ek array return karni hai wihtout sorted element but then no of
+#return krna hai kitni baar element repreat hua hai and yeah out of index jayega 
+
+        
