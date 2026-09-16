997fabd736e700747915878be958abc1dfd95508
Atharva Singh <Atharvasingh214@gmail.com>
2026-09-16 10:41:55 +0530

    Time: 2189 ms (12.06%), Space: 13.2 MB (37.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -13,4 +13,12 @@ class Solution(object):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
                     return i,j
-        
+        '''i=0 (nums[0]=3):
+j=1: 3+5=8 ≠ 9
+j=2: 3+2=5 ≠ 9
+j=3: 3+7=10 ≠ 9
+i=1 (nums[1]=5):
+j=2: 5+2=7 ≠ 9
+j=3: 5+7=12 ≠ 9
+i=2 (nums[2]=2):
+j=3: 2+7=9 ✅ match! → return [2,3]'''
