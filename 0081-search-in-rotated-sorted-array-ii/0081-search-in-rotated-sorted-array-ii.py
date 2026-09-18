02bea329b3e325820e65aea2867cb71a8162feeb
Atharva Singh <Atharvasingh214@gmail.com>
2026-09-19 00:19:41 +0530

    Time: 2 ms (11.23%), Space: 12.8 MB (6.08%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -9,4 +9,22 @@ class Solution(object):
             if nums[i]==target:
                 return True
         return False
-        
+        #but we have to do this in binary search to reduce the time complexity 
+        ''' lo=0
+            hi=len(nums)-1
+            while(lo<=hi):
+                mid=(lo+hi)//2
+                if nums[mid]==target:
+                    return True
+                elif mid<target:
+                    lo=mid+1
+                else:
+                    hi=mid-1
+            return False
+            def recursuve(target,nums,start):
+                n=len(nums)
+                if start>len(nums):
+                    return False
+                if nums(start)==target:
+                    return True
+                recusuve(target,nums,start+1)'''
