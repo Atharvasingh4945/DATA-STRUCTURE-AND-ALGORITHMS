b195cf921953c20f30b185efa4491c2773861688
Atharva Singh <Atharvasingh214@gmail.com>
2026-09-26 10:42:22 +0530

    Time: 2143 ms (26.34%), Space: 13.2 MB (55.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -10,15 +10,15 @@ class Solution(object):
                 return i,i+1'''
                 #this is error 1)indexout of bound 2)only taking adjacent inexes
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            for j in range(i+1,len(nums)):#sliding window
                 if nums[i]+nums[j]==target:
                     return i,j
         '''i=0 (nums[0]=3):
-j=1: 3+5=8 ≠ 9
-j=2: 3+2=5 ≠ 9
-j=3: 3+7=10 ≠ 9
-i=1 (nums[1]=5):
-j=2: 5+2=7 ≠ 9
-j=3: 5+7=12 ≠ 9
-i=2 (nums[2]=2):
-j=3: 2+7=9 ✅ match! → return [2,3]'''
+        j=1: 3+5=8 ≠ 9
+        j=2: 3+2=5 ≠ 9
+        j=3: 3+7=10 ≠ 9
+        i=1 (nums[1]=5):
+        j=2: 5+2=7 ≠ 9
+        j=3: 5+7=12 ≠ 9
+        i=2 (nums[2]=2):
+        j=3: 2+7=9 ✅ match! → return [2,3]'''
